589e2199d51ac085c814e1b91ea68d1d3723bd18
pfhousonix-lab <pfhousonix@gmail.com>
2025-10-30 17:45:03 +0800

    Create hexagram_weights.py

---
+++ hexagram_weights.py
@@ -0,0 +1,401 @@
+hexagram_weights = {
+    "乾下乾上": {
+        "蒙眼幫": {"double": +10, "red": +2.0, "ascend": +3.0},
+        "眼鏡幫": {"double": +4, "red": +2.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -3, "red": -0.5, "ascend": +3.5},
+        "不入幫": {"double": -6, "red": -1.5, "ascend": -2.0}
+    },
+    "坤下坤上": {
+        "蒙眼幫": {"double": -5, "red": -1.0, "ascend": -1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.0},
+        "鐮刀幫": {"double": +2, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -2, "red": -0.5, "ascend": -1.0}
+    },
+    "坎下震上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "艮下坎上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坎下乾上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下坎上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "坤下坎上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坎下坤上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "巽下乾上": {
+        "蒙眼幫": {"double": +4, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +3, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下兌上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    }
+}
+
+hexagram_weights.update({
+    "坤下乾上": {
+        "蒙眼幫": {"double": +8, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.5},
+        "不入幫": {"double": -4, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下坤上": {
+        "蒙眼幫": {"double": -4, "red": -1.0, "ascend": -1.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -2, "red": -0.5, "ascend": -1.0}
+    },
+    "乾下離上": {
+        "蒙眼幫": {"double": +5, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +4, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下乾上": {
+        "蒙眼幫": {"double": +3, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +2, "red": +0.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -4, "red": -1.0, "ascend": -1.0}
+    },
+    "坤下艮上": {
+        "蒙眼幫": {"double": -3, "red": -0.5, "ascend": -1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "震下坤上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "兌下震上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "艮下巽上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坤下兌上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "巽下坤上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    }
+})
+
+hexagram_weights.update({
+    "離下震上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "艮下離上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "艮下坤上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坤下震上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "乾下震上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "艮下乾上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "艮下震上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "兌下巽上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "坎下坎上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "離下離上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    }
+})
+
+
+hexagram_weights.update({
+    "兌下艮上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "震下巽上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下艮上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "震下乾上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下坤上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坤下離上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "巽下離上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "離下兌上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "坎下艮上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "震下坎上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    }
+})
+
+hexagram_weights.update({
+    "艮下坤上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "巽下震上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下兌上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "乾下巽上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "坤下兌上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坤下巽上": {
+        "蒙眼幫": {"double": -3, "red": -1.0, "ascend": -0.5},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.0},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +1.5},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "坎下兌上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "坎下巽上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下兌上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下巽上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    }
+})
+hexagram_weights.update({
+    "震下震上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "艮下艮上": {
+        "蒙眼幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": +1, "red": +0.5, "ascend": +2.0},
+        "不入幫": {"double": -1, "red": -0.5, "ascend": -1.0}
+    },
+    "巽下艮上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "兌下巽上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "震下離上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下震上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "巽下巽上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "兌下兌上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "巽下坎上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "坎下巽上": {
+        "蒙眼幫": {"double": +1, "red": +0.5, "ascend": +1.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "兌下離上": {
+        "蒙眼幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "眼鏡幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "震下艮上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    },
+    "坎下離上": {
+        "蒙眼幫": {"double": +4, "red": +2.0, "ascend": +2.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +1.5},
+        "鐮刀幫": {"double": -2, "red": -0.5, "ascend": +1.0},
+        "不入幫": {"double": -3, "red": -1.0, "ascend": -1.5}
+    },
+    "離下坎上": {
+        "蒙眼幫": {"double": +2, "red": +1.0, "ascend": +1.5},
+        "眼鏡幫": {"double": +3, "red": +1.5, "ascend": +2.0},
+        "鐮刀幫": {"double": -1, "red": -0.5, "ascend": +0.5},
+        "不入幫": {"double": -2, "red": -1.0, "ascend": -1.0}
+    }
+})
